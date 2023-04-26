2683f9bb3716e009081fb506555c1abf8fdea7aa
Tj_ <tjvishwa88@gmail.com>
2023-04-26 13:28:19 +0530

    Set up all the albums andalbumslistitem using RTK query technique now for practice adding photosList using same RTK query method

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,5 +1,46 @@
-function AlbumsList({user}){
-   return <div>Albums for {user.name}</div>
+import { useFetchAlbumsQuery,useAddAlbumMutation } from "../store";
+import Skeleton from "./Skeleton";
+import Button from "./Button";
+import AlbumsListItem from "./AlbumsListItem";
+
+function AlbumsList({ user }){
+   const { data, error, isFetching } = useFetchAlbumsQuery(user);
+   const [addAlbum, results] = useAddAlbumMutation();
+   
+   const handleAddAlbum = () => {
+      addAlbum(user);
+   }
+
+   let content;
+   if(isFetching){
+      content = <Skeleton className="h-10 w-full" times={3}/>
+   }else if(error) {
+      content = <div>Error loading albums</div>
+   }else{
+      content = data.map(album => {
+         return <AlbumsListItem key={album.id} album={album} />
+         
+      })
+   }
+   return <div>
+            <div className="m-2 flex flex-row items-center justify-between">
+               <h3 className="text-lg font-bold"> Albums for {user.name} </h3>
+               <Button loading={results.isLoading} onClick={handleAddAlbum}>
+                  + Add Album
+               </Button>
+            </div>
+            <div>
+               {content}
+            </div>
+          </div>
 }
 
 export default AlbumsList;
+
+
+
+
+
+/// Difference between isLoading and isFetching is
+// isLoading -- set true only for the first time when you make a request/
+// isFetching -- is set to true everytime you make a request
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { usersReducer } from "./slices/usersSlice";
-
+import { albumsApi } from './apis/albumsApi';
 
 export const store = configureStore({
     reducer:{
-        users: usersReducer
+        users: usersReducer,
+        [albumsApi.reducerPath]: albumsApi.reducer
+        //here albumsApi.reducerPath is going to returnthe string
+        // in that path
+    },
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware()
+        .concat(albumsApi.middleware);
     }
-})
+});
+
+
+setupListeners(store.dispatch);
 
 export * from './thunk/fetchUsers';
 export * from './thunk/addUser';
 export * from './thunk/removeUser';
+
+export { useFetchAlbumsQuery,
+         useAddAlbumMutation,
+        useRemoveAlbumMutation
+} from './apis/albumsApi';
